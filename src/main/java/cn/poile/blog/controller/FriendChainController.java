f27c9ced237ca4a9c459e59b7f240aa835ac076a
yaohw007 <726856005@qq.com>
2020-01-10 16:21:02 +0800

    rename

--- src/main/java/cn/poile/blog/controller/FriendChainController.java
+++ src/main/java/cn/poile/blog/controller/FriendChainController.java
@@ -3,7 +3,7 @@ package cn.poile.blog.controller;
 
 import cn.poile.blog.common.oss.Storage;
 import cn.poile.blog.common.response.ApiResponse;
-import cn.poile.blog.entity.FriendLink;
+import cn.poile.blog.entity.FriendChain;
 import cn.poile.blog.service.IFriendLinkService;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
@@ -27,7 +27,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/friend/link")
 @Api(tags = "友链服务", value = "/friend/link")
-public class FriendLinkController extends BaseController {
+public class FriendChainController extends BaseController {
 
     @Autowired
     private IFriendLinkService friendLinkService;
@@ -37,38 +37,43 @@ public class FriendLinkController extends BaseController {
 
     @PostMapping("/save")
     @ApiOperation(value = "新增或更新友链", notes = "id不为空时更新，需要accessToken，需要管理员权限")
-    public ApiResponse add(@RequestBody FriendLink friendLink) {
-        friendLinkService.saveOrUpdate(friendLink);
+    public ApiResponse add(@RequestBody FriendChain friendChain) {
+        friendLinkService.saveOrUpdate(friendChain);
         return createResponse();
     }
 
     @GetMapping("/list")
     @ApiOperation(value = "获取友链列表")
-    public ApiResponse<List<FriendLink>> list() {
+    public ApiResponse<List<FriendChain>> list() {
         return createResponse(friendLinkService.list());
     }
 
     @GetMapping("/page")
     @ApiOperation(value = "分页获取友链列表")
-    public ApiResponse<IPage<FriendLink>> page(
+    public ApiResponse<IPage<FriendChain>> page(
             @ApiParam("页码") @RequestParam(value = "current", required = false, defaultValue = "1") long current,
             @ApiParam("每页数量") @RequestParam(value = "size", required = false, defaultValue = "5") long size) {
-        Page<FriendLink> page = new Page<>(current, size);
+        Page<FriendChain> page = new Page<>(current, size);
         return createResponse(friendLinkService.page(page));
     }
 
     @DeleteMapping("/delete/{id}")
     @ApiOperation(value = "删除友链")
     public ApiResponse delete(@ApiParam("友链id") @PathVariable(value = "id") int id) {
-        FriendLink friendLink = friendLinkService.getById(id);
-        if (friendLink != null) {
-            friendLinkService.removeById(id);
-            String icon = friendLink.getIcon();
-            if (!StringUtils.isBlank(icon)) {
-                storage.delete(icon);
-            }
+        FriendChain friendChain = friendLinkService.getById(id);
+        if (friendChain != null && StringUtils.isNotBlank(friendChain.getIcon())) {
+            deleteIcon(friendChain.getIcon());
         }
+        friendLinkService.removeById(id);
         return createResponse();
+    }
+
+    /**
+     * 删除icon
+     * @param path
+     */
+    private void deleteIcon(String path) {
+        storage.delete(path);
     }
 
 }
