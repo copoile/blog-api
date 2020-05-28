f0b41ac3cc28a1c8b063db275b85997b3ca30041
yaohw007 <726856005@qq.com>
2020-05-28 10:37:05 +0800

    QQ第三方登录优化

--- src/main/java/cn/poile/blog/common/oauth/QQThirdAuth.java
+++ src/main/java/cn/poile/blog/common/oauth/QQThirdAuth.java
@@ -15,12 +15,13 @@ import java.util.Map;
 
 /**
  * QQ 第三方登录
+ *
  * @author: yaohw
  * @create: 2020-05-21 18:36
  **/
 @Service
 @Log4j2
-public class QQThirdAuth{
+public class QQThirdAuth {
 
     @Value("${oauth.qq.appId}")
     public String appId;
@@ -40,20 +41,22 @@ public class QQThirdAuth{
      * @param accessToken
      * @return
      */
-    public ThirdAuthUser getUserInfo(String accessToken,String openid) {
-        Map<String,String> params = new HashMap<>(2);
+    public ThirdAuthUser getUserInfo(String accessToken, String openid) {
+        Map<String, String> params = new HashMap<>(4);
         params.put("access_token", accessToken);
         params.put("openid", openid);
-        params.put("oauth_consumer_key",appId);
+        params.put("oauth_consumer_key", appId);
         String result = HttpClientUtil.doGet(OauthConstant.QQ_ACCESS_USER_URL, params);
         if (StringUtils.isBlank(result)) {
-            throw new ApiException(ErrorEnum.SYSTEM_ERROR.getErrorCode(),"获取第三方用户信息出错");
+            throw new ApiException(ErrorEnum.SYSTEM_ERROR.getErrorCode(), "获取第三方用户信息出错");
         }
         JSONObject jsonObject = JSON.parseObject(result);
         ThirdAuthUser thirdAuthUser = new ThirdAuthUser();
         thirdAuthUser.setUuid(openid);
         thirdAuthUser.setNickname(jsonObject.getString("nickname"));
-        thirdAuthUser.setAvatar(jsonObject.getString("figureurl_1"));
+        // 头像取100*100的，但100*100的不一定都有
+        String avatar = StringUtils.isBlank(jsonObject.getString("figureurl_qq_2")) ? jsonObject.getString("figureurl_qq_1") : jsonObject.getString("figureurl_qq_2");
+        thirdAuthUser.setAvatar(avatar);
         return thirdAuthUser;
     }
 
@@ -64,15 +67,15 @@ public class QQThirdAuth{
      * @return
      */
     public ThirdAuthToken getAuthToken(String code) {
-        Map<String,String> params = new HashMap<>(8);
-        params.put("grant_type","authorization_code");
+        Map<String, String> params = new HashMap<>(8);
+        params.put("grant_type", "authorization_code");
         params.put("client_id", clientId);
         params.put("client_secret", clientSecret);
-        params.put("code",code);
+        params.put("code", code);
         params.put("redirect_uri", redirect);
         String result = HttpClientUtil.doGet(OauthConstant.QQ_ACCESS_TOKE_URL, params);
         if (StringUtils.isBlank(result)) {
-            throw new ApiException(ErrorEnum.SYSTEM_ERROR.getErrorCode(),"获取第三方token出错");
+            throw new ApiException(ErrorEnum.SYSTEM_ERROR.getErrorCode(), "获取第三方token出错");
         }
         ThirdAuthToken thirdAuthToken = new ThirdAuthToken();
         thirdAuthToken.setAccessToken(getAccessToken(result));
@@ -81,27 +84,26 @@ public class QQThirdAuth{
 
     /**
      * 获取openid
+     *
      * @param accessToken
      * @return
      */
     public String getOpenid(String accessToken) {
-        Map<String,String> params = new HashMap<>(2);
+        Map<String, String> params = new HashMap<>(2);
         params.put("access_token", accessToken);
         String result = HttpClientUtil.doGet(OauthConstant.QQ_ACCESS_OPENID_URL, params);
         if (StringUtils.isBlank(result)) {
-            throw new ApiException(ErrorEnum.SYSTEM_ERROR.getErrorCode(),"获取openid出错");
+            throw new ApiException(ErrorEnum.SYSTEM_ERROR.getErrorCode(), "获取openid出错");
         }
         // callback( {"client_id":"YOUR_APPID","openid":"YOUR_OPENID"} );
-        String json = result.replace("callback","")
-                .replace("(","")
-                .replace(");","")
-                .replace(")","");
+        String json = result.replaceAll("(callback)|(\\()|(\\))|(\\))|(;)", "");
         JSONObject jsonObject = JSON.parseObject(json);
         return jsonObject.getString("openid");
     }
 
     /**
      * 获取 access_token
+     *
      * @param result
      * @return
      */
@@ -110,9 +112,9 @@ public class QQThirdAuth{
         // access_token=aa5a59cd212b2c0f3c1f285822b2085f52fe3850&scope=user%3Aemail&token_type=bearer
         try {
             return result.split("&")[0].split("=")[1];
-        }catch (ArrayIndexOutOfBoundsException e) {
+        } catch (ArrayIndexOutOfBoundsException e) {
             log.error("获取access_token异常", e);
-            throw new ApiException(ErrorEnum.SYSTEM_ERROR.getErrorCode(),"获取第三方token出错");
+            throw new ApiException(ErrorEnum.SYSTEM_ERROR.getErrorCode(), "获取第三方token出错");
         }
     }
 }
